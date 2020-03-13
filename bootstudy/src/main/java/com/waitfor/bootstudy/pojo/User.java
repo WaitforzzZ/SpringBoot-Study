50de65dcffe5e954523477689c68f596237192a1
WaitforzzZ <1204918135@qq.com>
2020-03-13 15:05:01 +0800

    20200313_Jackson的基本演绎

--- bootstudy/src/main/java/com/waitfor/bootstudy/pojo/User.java
+++ bootstudy/src/main/java/com/waitfor/bootstudy/pojo/User.java
@@ -2,16 +2,24 @@ package com.waitfor.bootstudy.pojo;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 public class User {
 	
 	private String name;
 	
+	@JsonIgnore
 	private String password;
 	
 	private Integer age;
 	
+	@JsonFormat(pattern="yyyy-MM-dd hh:mm:ss a", locale="zh", timezone="GMT+8")
 	private Date birthday;
 	
+	@JsonInclude(Include.NON_NULL)
 	private String desc;
 
 	public String getName() {
